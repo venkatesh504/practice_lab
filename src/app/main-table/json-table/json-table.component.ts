2d6a10693decc6a92fbd033ebaaf73b7cda2fd4c
Venkatesh Y <venkatesh.y@way2online.co.in>
2019-03-28 16:38:24 +0530

    json Data Add

--- src/app/main-table/json-table/json-table.component.ts
+++ src/app/main-table/json-table/json-table.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import data   from  '../../../assets/data.json';
+import MOCK_DATA from '../../../assets/MOCK_DATA.json';
+import solo_data from '../../../assets/solo_data.json'
 
 @Component({
   selector: 'app-json-table',
@@ -7,7 +9,8 @@ import data   from  '../../../assets/data.json';
   styleUrls: ['./json-table.component.scss']
 })
 export class JsonTableComponent implements OnInit {
-
+totalData :any;
+solo_Data: any;
 
   myData:any;
   constructor() { 
@@ -16,7 +19,8 @@ export class JsonTableComponent implements OnInit {
 
   ngOnInit() {
 this.myData = data;
-
+this.totalData = MOCK_DATA;
+this.solo_Data = solo_data;
 console.log(this.myData);
   }
 
